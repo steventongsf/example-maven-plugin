277b8af722237ea6fe962648508115488670208b
Steven Tong <steventongsf@gmail.com>
2021-09-29 00:17:41 -0700

    initial commit

---
+++ src/main/java/com/stong/plugins/GitVersionMojo.java
@@ -0,0 +1,33 @@
+package com.stong.plugins;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+import javax.inject.Inject;
+
+@Mojo(name = "version", defaultPhase = LifecyclePhase.INITIALIZE)
+public class GitVersionMojo extends AbstractMojo {
+
+    /**
+     * The git command used to retrieve the current commit hash.
+     */
+    @Parameter(property = "git.command", defaultValue = "git rev-parse --short HEAD")
+    private String command;
+
+    @Parameter(property = "project", readonly = true)
+    private MavenProject project;
+
+    @Inject
+    private VersionProvider versionProvider;
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        String version = versionProvider.getVersion(command);
+        project.getProperties().put("exampleVersion", version);
+        getLog().info("Git hash: " + version);
+    }
+}
